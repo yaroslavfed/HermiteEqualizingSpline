7915e821b9b15e7d6fd168d09ad87cf20be92bc8
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2023-12-19 21:11:25 +0700

    Разбил на конечные элементы

--- HermiteEqualizingSpline/Program.cs
+++ HermiteEqualizingSpline/Program.cs
@@ -14,39 +14,9 @@ internal class Program
     private static Area s_inputData = new();
     private static Area s_outputData = new();
 
-    private static double weight = 1;
-    private static double a = 0;
-    private static double b = 0.001;
-
-    private static IList<Node> list = new List<Node>()
-    {
-        new() { X = 0, Y = 10.1 },
-        new() { X = 0.1, Y = 9.4 },
-        new() { X = 0.2, Y = 11.2 },
-        new() { X = 0.3, Y = 8.7 },
-        new() { X = 0.4, Y = 10.6 },
-        new() { X = 0.5, Y = 9.2 },
-        new() { X = 0.6, Y = 10.8 },
-        new() { X = 0.7, Y = 8.5 },
-        new() { X = 0.8, Y = 9.5 },
-        new() { X = 0.9, Y = 9.2 },
-        new() { X = 1, Y = 7.4 },
-        new() { X = 1.1, Y = 7.2 },
-        new() { X = 1.2, Y = 5.5 },
-        new() { X = 1.3, Y = 5.5 },
-        new() { X = 1.4, Y = 4.1 },
-        new() { X = 1.5, Y = 3.7 },
-        new() { X = 1.6, Y = 2.2 },
-        new() { X = 1.7, Y = 1.9 },
-        new() { X = 1.8, Y = 1.4 },
-        new() { X = 1.9, Y = 1.3 },
-        new() { X = 2, Y = 1.15 },
-        new() { X = 2.1, Y = 0.85 },
-        new() { X = 2.2, Y = 1.2 },
-        new() { X = 2.3, Y = 1.02 },
-        new() { X = 2.4, Y = 0.8 },
-        new() { X = 2.5, Y = 1 },
-    };
+    private static readonly double s_weight = 1;
+    private static readonly int s_finiteElementsCount = 5;
+    private static readonly int s_nodesPerElementCount = 4;
 
     #endregion
 
@@ -54,7 +24,7 @@ internal class Program
 
     static void Main(string[] args)
     {
-        s_inputData = ReadDataFile();
+        s_inputData = SplitAreaToFiniteElements();
         s_outputData = ResolveSpline();
 
         DrawPlot();
@@ -64,19 +34,18 @@ internal class Program
 
     #region Methods
 
-    static Area ReadDataFile()
+    static Area SplitAreaToFiniteElements()
     {
-        string fileName = s_configurationString;
-        string jsonString = File.ReadAllText(fileName);
-
-        var nodeList = JsonSerializer.Deserialize<FiniteElement>(jsonString)!;
+        var nodesList = ReadDataFile();
+        var nodesCount = s_finiteElementsCount <= 1 ? s_nodesPerElementCount : nodesList.Count;
 
         var elements = new List<FiniteElement>();
-        for (int i = 0; i < nodeList.Nodes.Count; i += 3)
+        for (int i = 0; i < s_finiteElementsCount; i += nodesList.Count - 1)
         {
+            var temp = nodesList.Skip(i).Take(s_nodesPerElementCount).ToList();
             var element = new FiniteElement
             {
-                Nodes = nodeList.Nodes.Skip(i).Take(4).ToList()
+                Nodes = temp
             };
             elements.Add(element);
         }
@@ -87,12 +56,21 @@ internal class Program
         };
     }
 
+    static IList<Node> ReadDataFile()
+    {
+        var fileName = s_configurationString;
+        var jsonString = File.ReadAllText(fileName);
+        var data = JsonSerializer.Deserialize<FiniteElement>(jsonString) ?? new FiniteElement();
+
+        return data.Nodes;
+    }
+
     static Area ResolveSpline()
     {
         var A = GetAMatrix();
         var b = GetBVector();
 
-        var q = Gauss(2, A, b);
+        var q = Gauss(A.Count, A, b);
         var S = GetSpline(q);
 
 
@@ -115,21 +93,24 @@ internal class Program
         IList<Node> result = new List<Node>();
         var node = new Node();
 
-        for (double i = list.FirstOrDefault()!.X; i < list.LastOrDefault()!.X; i += 0.01)
+        foreach (var element in s_inputData.Elements)
         {
-            double item = 0;
-            for (int j = 0; j < q.Count; j++)
+            for (double i = element.Nodes.FirstOrDefault()!.X; i < element.Nodes.LastOrDefault()!.X; i += 0.01)
             {
-                item += q[j] * GetBasicFunctions(j, i, list.FirstOrDefault()!.X,
-                    list.LastOrDefault()!.X - list.FirstOrDefault()!.X);
-            }
-
-            result.Add(
-                new Node
+                double item = 0;
+                for (int j = 0; j < q.Count; j++)
                 {
-                    X = i,
-                    Y = item
-                });
+                    item += q[j] * GetBasicFunctions(j, i, element.Nodes.FirstOrDefault()!.X,
+                        element.Nodes.LastOrDefault()!.X - element.Nodes.FirstOrDefault()!.X);
+                }
+
+                result.Add(
+                    new Node
+                    {
+                        X = i,
+                        Y = item
+                    });
+            }
         }
 
         return result;
@@ -138,27 +119,30 @@ internal class Program
     static IList<IList<double>> GetAMatrix()
     {
         var iList = new List<IList<double>>();
-        for (int i = 0; i < 2; i++)
+        foreach (var element in s_inputData.Elements)
         {
-            var jList = new List<double>();
-            for (int j = 0; j < 2; j++)
+            for (int i = 0; i < 2 * s_inputData.Elements.Count; i++)
             {
-                double result = 0;
-                for (int k = 0; k < list.Count; k++)
+                var jList = new List<double>();
+                for (int j = 0; j < 2 * s_inputData.Elements.Count; j++)
                 {
-                    result += weight
-                              * GetBasicFunctions(i,
-                                  list[k].X, list.FirstOrDefault()!.X,
-                                  list.LastOrDefault()!.X - list.FirstOrDefault()!.X)
-                              * GetBasicFunctions(j,
-                                  list[k].X, list.FirstOrDefault()!.X,
-                                  list.LastOrDefault()!.X - list.FirstOrDefault()!.X);
+                    double result = 0;
+                    for (int k = 0; k < element.Nodes.Count; k++)
+                    {
+                        result += s_weight
+                                  * GetBasicFunctions(i,
+                                      element.Nodes[k].X, element.Nodes.FirstOrDefault()!.X,
+                                      element.Nodes.LastOrDefault()!.X - element.Nodes.FirstOrDefault()!.X)
+                                  * GetBasicFunctions(j,
+                                      element.Nodes[k].X, element.Nodes.FirstOrDefault()!.X,
+                                      element.Nodes.LastOrDefault()!.X - element.Nodes.FirstOrDefault()!.X);
+                    }
+
+                    jList.Add(result);
                 }
 
-                jList.Add(result);
+                iList.Add(jList);
             }
-
-            iList.Add(jList);
         }
 
         return iList;
@@ -167,20 +151,58 @@ internal class Program
     static IList<double> GetBVector()
     {
         var fList = new List<double>();
-        for (int i = 0; i < 2; i++)
+        foreach (var element in s_inputData.Elements)
         {
-            double result = 0;
-            for (int k = 0; k < list.Count; k++)
+            for (int i = 0; i < 2 * s_inputData.Elements.Count; i++)
             {
-                result += weight * list[k].Y * GetBasicFunctions(i,
-                    list[k].X, list.FirstOrDefault()!.X,
-                    list.LastOrDefault()!.X - list.FirstOrDefault()!.X);
-            }
+                double result = 0;
+                for (int k = 0; k < element.Nodes.Count; k++)
+                {
+                    result += s_weight * element.Nodes[k].Y * GetBasicFunctions(i,
+                        element.Nodes[k].X, element.Nodes.FirstOrDefault()!.X,
+                        element.Nodes.LastOrDefault()!.X - element.Nodes.FirstOrDefault()!.X);
+                }
 
-            fList.Add(result);
+                fList.Add(result);
+            }
         }
 
         return fList;
+    }
+
+    static List<double> Gauss(int N, IList<IList<double>> B, IList<double> RightPart)
+    {
+        double[] x = new double[N];
+        double R;
+
+        // Прямой ход
+        for (int q = 0; q < N; q++)
+        {
+            R = 1 / B[q][q];
+            B[q][q] = 1;
+            for (int j = q + 1; j < N; j++)
+                B[q][j] *= R;
+            RightPart[q] *= R;
+            for (int k = q + 1; k < N; k++)
+            {
+                R = B[k][q];
+                B[k][q] = 0;
+                for (int j = q + 1; j < N; j++)
+                    B[k][j] = B[k][j] - B[q][j] * R;
+                RightPart[k] -= RightPart[q] * R;
+            }
+        }
+
+        // Обратный ход
+        for (int q = N - 1; q >= 0; q--)
+        {
+            R = RightPart[q];
+            for (int j = q + 1; j < N; j++)
+                R -= B[q][j] * x[j];
+            x[q] = R;
+        }
+
+        return x.ToList();
     }
 
     static double GetBasicFunctions(int i, double x, double xi, double h)
@@ -254,41 +276,6 @@ internal class Program
         }
 
         sw.WriteLine();
-    }
-
-    static List<double> Gauss(int N, IList<IList<double>> B, IList<double> RightPart)
-    {
-        double[] x = new double[N];
-        double R;
-
-        // Прямой ход
-        for (int q = 0; q < N; q++)
-        {
-            R = 1 / B[q][q];
-            B[q][q] = 1;
-            for (int j = q + 1; j < N; j++)
-                B[q][j] *= R;
-            RightPart[q] *= R;
-            for (int k = q + 1; k < N; k++)
-            {
-                R = B[k][q];
-                B[k][q] = 0;
-                for (int j = q + 1; j < N; j++)
-                    B[k][j] = B[k][j] - B[q][j] * R;
-                RightPart[k] -= RightPart[q] * R;
-            }
-        }
-
-        // Обратный ход
-        for (int q = N - 1; q >= 0; q--)
-        {
-            R = RightPart[q];
-            for (int j = q + 1; j < N; j++)
-                R -= B[q][j] * x[j];
-            x[q] = R;
-        }
-
-        return x.ToList();
     }
 
     #endregion
